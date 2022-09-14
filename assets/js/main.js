c9bd0c0974a0c87b297fbbdeeb887cab08ecc743
Denis K <adkalashnikov@gmail.com>
2022-09-14 11:47:53 +0300

    updating front page

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($) {
+	const headerHeight = $('.header').outerHeight();
+	
+	$(".banner,.main-section").css("margin-top" , headerHeight);
+
+	$(window).scroll(function(){
+	    if ($(window).scrollTop() >= 10) {
+	        $('.header').addClass('fixed-header');
+	    }
+	    else {
+	        $('.header').removeClass('fixed-header');
+	    }
+	}).scroll();
+});
+
+// init animation lib
+setTimeout(() => {
+    AOS.init({
+        once: true,
+        duration: 500,
+        disable() {
+            return window.innerWidth < 992;
+        }
+    });
+}, 0);
